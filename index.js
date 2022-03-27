ab3bcd04d6d371607a0771a3a834f5271dbdd966
Michael Warren <mwarrn@amazon.com>
2022-03-27 09:00:00 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = [];
+
+const returnFirstTwoDrivers = function(drivers) {
+    const newDrivers = [...drivers];
+    return newDrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    const newDrivers = [...drivers];
+    return newDrivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+    return function(value) {
+        return multiplier * value;
+    }
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnDriversFunction) {
+    return returnDriversFunction(drivers);
+}
